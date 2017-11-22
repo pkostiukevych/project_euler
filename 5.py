52b4a2826a86bdfb58f93969a756dfce334910c2
pkostiukevych <pkostiukevych@users..github.com>
2017-11-22 16:52:16 +0200

    5

--- 5.py
+++ 5.py
@@ -1,27 +1,23 @@
-import math
+from math import floor, log, sqrt
+from itertools import count, islice
 
-def gen_primes(lim):
-    primes = [2]
-    for i in range(3, lim, 2):
-        is_prime= True
-        j = 0
-        while (primes[j] * primes[j] <= i):
-            if i % primes[j] == 0:
-                is_prime = False
-                break
-            j += 1
+def is_prime(n):
+    return n > 1 and all(n % i for i in islice(count(2), floor(sqrt(n)-1)))
 
-        if is_prime:
-            primes.append(i)
-    return primes
+def prime_generator(stop=10001):
+    n = 2
+    counter = 1
+    while counter <= stop:
+        if is_prime(n):
+            yield n
+            counter += 1
+        n += 1
 
 def find_number(div_max=20):
-    primes = gen_primes(div_max)
-
     result = 1
 
-    for prime in primes:
-        a = math.floor(math.log(div_max) / math.log(prime))
+    for prime in prime_generator():
+        a = floor(log(div_max) / log(prime))
         result = result * int(pow(prime, a))
 
     return result
